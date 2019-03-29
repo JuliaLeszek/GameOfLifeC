f1057b8dc8617d68e379ad579ebbf77b785435ce
Julia Leszek <julialeszek93@gmail.com>
2019-03-30 00:40:45 +0100

    sasiedztwo 1'

---
+++ neighbourhood.c
@@ -0,0 +1,15 @@
+//
+// Created by julia on 29.03.19.
+//
+
+#include "neighbourhood.h"
+#include "generation.h"
+
+int alive_neighbour_count(generation_t* grid, int i, int j){
+    int m,n;
+    int count = 0;
+    for(m = i - 1; m = i + 1; m++) {
+
+    }
+
+}
---
+++ neighbourhood.h
@@ -0,0 +1,12 @@
+//
+// Created by julia on 29.03.19.
+//
+
+#ifndef GAMEOFLIFE_NEIGHBOURHOOD_H
+#define GAMEOFLIFE_NEIGHBOURHOOD_H
+
+#include "generation.h"
+
+int alive_neighbour_count(generation_t* grid, int i, int j);
+
+#endif //GAMEOFLIFE_NEIGHBOURHOOD_H
