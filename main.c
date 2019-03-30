b303ec7a8147a5957e05b06039f1380980d4c052
Julia Leszek <julialeszek93@gmail.com>
2019-03-30 20:47:40 +0100

    zyjacy sasiedzi + test zliczania

--- main.c
+++ main.c
@@ -2,14 +2,19 @@
 #include "cell.h"
 #include "rules.h"
 #include "generation.h"
+#include "neighbourhood.h"
 
 
 int main() {
 
     generation_t* p = create_generation( 100, 100);
     cell_t *c = cell (p, 10, 7);
+    cell_t *e = cell (p, 10, 9);
     change_state (c, rules ( 3, get_state(c)));
+    change_state (e, rules ( 3, get_state(c)));
+    int d = alive_neighbour_count(p, 10, 8);
     printf("c = %d\n", (int) c->state);
+    printf("d = %d\n", d);  // zlicza! <3
     free_gen(p);
 
     return 0;
--- neighbourhood.c
+++ neighbourhood.c
@@ -6,10 +6,19 @@
 #include "generation.h"
 
 int alive_neighbour_count(generation_t* grid, int i, int j){
-    int m,n;
-    int count = 0;
-    for(m = i - 1; m = i + 1; m++) {
-
+    int m, n, count = 0;
+    for (m = i - 1; m <= i + 1; m++) {
+        if (m < 0 || m >= grid->height){
+            continue;
+        }
+        for (n = j - 1; n <= j + 1; n++){
+            if (n < 0 || n >= grid->width || (m == i && n == j)){
+                continue;
+            }
+            if (get_state(cell(grid, m, n)) == ALIVE){
+                count++;
+            }
+        }
     }
-
+    return count;
 }
